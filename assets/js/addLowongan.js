50611a3c83c69eb99ea669c616f2740d4036fb54
adityamaulanazidqy <adityamaullana234@gmail.com>
2023-12-02 10:46:35 +0700

    .

--- assets/js/addLowongan.js
+++ assets/js/addLowongan.js
@@ -1,23 +1,11 @@
 // Function to display the popup
 function showPopup() {
-  // var popup = document.getElementById('popup');
-  // popup.style.display = 'block';
-  Swal.fire({
-    icon: 'error',
-    title: 'Oops...',
-    text: 'Something went wrong!',
-    footer: '<a href="#">Why do I have this issue?</a>',
-  });
+  var popup = document.getElementById('popup');
+  popup.style.display = 'block';
 }
 
 // Function to close the popup
 function closePopup() {
-  // var popup = document.getElementById('popup');
-  // popup.style.display = 'none';
-  Swal.fire({
-    icon: 'error',
-    title: 'Oops...',
-    text: 'Something went wrong!',
-    footer: '<a href="#">Why do I have this issue?</a>',
-  });
+  var popup = document.getElementById('popup');
+  popup.style.display = 'none';
 }
