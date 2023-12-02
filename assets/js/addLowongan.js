813ebf6b3b333268b3ec83cfaecff66f99f4a192
adityamaulanazidqy <adityamaullana234@gmail.com>
2023-12-02 10:33:15 +0700

    .

--- assets/js/addLowongan.js
+++ assets/js/addLowongan.js
@@ -2,6 +2,12 @@
 function showPopup() {
   var popup = document.getElementById('popup');
   popup.style.display = 'block';
+  Swal.fire({
+    icon: 'error',
+    title: 'Oops...',
+    text: 'Something went wrong!',
+    footer: '<a href="#">Why do I have this issue?</a>',
+  });
 }
 
 // Function to close the popup
