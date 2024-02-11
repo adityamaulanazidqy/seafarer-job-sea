d87759c5a42fe0b340ae1bd528c1e7dd4c22fc84
adityamaulanazidqy <adityamaullana234@gmail.com>
2024-02-11 20:17:44 +0700

    Koneksi

--- php/koneksi.php
+++ php/koneksi.php
@@ -1,16 +1,16 @@
 <?php
 
 // Domain
-// $hostname = "localhost";
-// $username = "u341255137_user";
-// $password = "sabiilyAhmaddinejat123";
-// $dbname = "u341255137_seafarerjobsea";
+$hostname = "localhost";
+$username = "u341255137_user";
+$password = "sabiilyAhmaddinejat123";
+$dbname = "u341255137_seafarerjobsea";
 
 // Xampp
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$dbname = "job_pelaut_indonesia";
+// $hostname = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "job_pelaut_indonesia";
 
 $conn = mysqli_connect($hostname, $username, $password, $dbname);
 if ($conn->connect_error) {
